cfd314e8bb58f43e823b163f0313059e8b118d2f
sawanon <sawanon.w@aapico.com>
2022-06-08 13:07:01 +0700

    express --save

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const importData = require("./data.json")
+let port = 3000
 
-app.get('/',(req,res)=>{
+app.get("/",(req,res)=>{
     res.send('C ya!')
 })
 
+app.get("/player",(req,res)=>{
+    res.send(importData)
+})
+
 app.listen(port, () =>{
-    console.log(`example app listening on port ${port}`);
+    console.log(`example app listening on port http://localhost:${port}`);
 })
